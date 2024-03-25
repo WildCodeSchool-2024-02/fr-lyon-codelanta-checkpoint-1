51b254561cb25cffe3596b2a411dbb4497702aa8
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-03-25 11:54:33 +0100

    almost finish js, still no idea why the image doesn't work

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,11 +4,17 @@ const efirstname = document.getElementById('firstname');
 const ebgColor = document.getElementsByClassName('pink-bg')[0];
 const allcolor = document.querySelectorAll('.pink-bg');
 const linka = document.querySelectorAll('a');
+const edevtools = document.getElementById('devtools');
+const etext1 = document.getElementById('text1');
+const etext2 = document.getElementById('text2');
+const etext3 = document.getElementById('text3');
+const toolsUl= document.getElementById('front-dev-tools');
+
 
 
 
 avatar.addEventListener("click", function() {
-    avatar.src = "./image/avatar-bis.png";
+    avatar.src = "../image/avatar-bis.png";
 });
 
 ebuttonName.addEventListener("click", function() {
@@ -25,4 +31,15 @@ ebuttonName.addEventListener("click", function() {
         acolor.style.color = color;
     });
 
+});
+
+
+edevtools.addEventListener("click", function() {
+    etext1.textContent = "lorem ipsilum 1";
+    etext2.textContent = "lorem ipsilum 2";
+    etext3.textContent = "lorem ipsilum 3";
+    const add = prompt("What else ?");
+    const newli = document.createElement("li");
+    newli.textContent = add;
+    toolsUl.appendChild(newli)
 });
