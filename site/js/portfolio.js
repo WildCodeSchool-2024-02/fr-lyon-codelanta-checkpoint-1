d8f85906e170a18bc5e3c02f40499351cf3ca298
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-03-25 12:56:28 +0100

    Finished optional JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,6 +4,13 @@ const firstname = document.getElementById('firstname')
 const pinkBackground = document.getElementsByClassName('pink-bg');
 const pinkText = document.getElementsByClassName('pink-text');
 const aBlack = document.getElementsByTagName('a');
+const frontSkills = document.getElementById('front-dev-tools');
+const frontButton = document.getElementById('modifyFront');
+const frontTools = frontSkills.getElementsByTagName('li')
+const backButton = document.getElementById("modifyBack");
+const backAdding = document.getElementById("addingBack");
+const backSkills = document.getElementById('back-dev-tools');
+// const backTools = backSkills.getElementsByTagName('li');
 
 /* ---------------- Changement d'Avatar --------------- */
 
@@ -33,5 +40,21 @@ buttonModify.addEventListener('click', () => {
     }
     firstname.textContent = prompt('Enter your name');
     firstname.style.color = "white";
+
+})
+
+frontButton.addEventListener('click', () => {
+    frontTools[0].textContent = "VSCode";
+    frontTools[1].textContent = "GitHub";
+    frontTools[2].textContent = "Terminal";
+})
+
+backButton.addEventListener('click', () => {
+    let newTool = document.createElement("li")
+    let backAddingvalue = backAdding.value;
+    console.log(backAddingvalue);
+    newTool.innerHTML = backAdding.value;
+    backSkills.appendChild(newTool);
+
 
 })
