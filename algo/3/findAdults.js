a86964b4298cb180bf44cb5c00cd9192442e2a2d
Joris Budin <jorisbudin@gmail.com>
2024-03-25 09:55:42 +0100

    finish algo 1 2 and 3 in algo

--- algo/3/findAdults.js
+++ algo/3/findAdults.js
@@ -24,9 +24,54 @@ const persons = [
 ];
 
 */
-
+const persons = [
+  { name: 'Mary Poppins', age: 32, sex: 'female' },
+  { name: 'Tony Stark', age: 36, sex: 'male' },
+  { name: 'John Doe', age: 12, sex: 'male' },
+  { name: 'Jane Doe', age: 6, sex: 'female' },
+  { name: 'Maggie Simpson', age: 2, sex: 'female' },
+  { name: 'Leonardo', age: 92, sex: 'male' },
+  { name: 'Carla Moreau', age: 24, sex: 'female' },
+  { name: 'Mickael Jackson', age: 17, sex: 'male' },
+  { name: 'Penelope Milena', age: 27, sex: 'female' },
+  { name: 'Homer Simpson', age: 45, sex: 'male' },
+  { name: 'Leonardo Di Caprio', age: 42, sex: 'male' },
+  { name: 'Carla Bruni', age: 54, sex: 'female' },
+  { name: 'Lisa Simpson', age: 13, sex: 'female' },
+  { name: 'Millie Bobby Brown', age: 15, sex: 'female' },
+  { name: 'Penelope Cruz', age: 47, sex: 'female' },
+];
+const persons1 = [
+  { name: "Mary Poppins", age: 32, sex: "female" },
+  { name: "Tony Stark", age: 36, sex: "male" },
+  { name: "John Doe", age: 12, sex: "male" },
+  { name: "Jane Doe", age: 6, sex: "female" },
+  { name: "Homer Simpson", age: 45, sex: "male" },
+  { name: "Leonardo Di Caprio", age: 42, sex: "male" },
+  { name: "Carla Bruni", age: 54, sex: "female" },
+  { name: "Lisa Simpson", age: 13, sex: "female" },
+  { name: "Millie Bobby Brown", age: 15, sex: "female" },
+  { name: "Penelope Cruz", age: 47, sex: "female" },
+]
+console.log(persons[0].sex)
 function findAdults(persons) {
-  // Your code here !
+  let femaleAdulte = [];
+  let femaleCount = 0;
+  let maleAdulte = []
+  let maleCount = 0
+  let answer = []
+  for (i in persons){
+    if(persons[i].sex === 'female' && persons[i].age >= 18){
+      femaleAdulte[femaleCount] = persons[i];
+      femaleCount++;
+    }else if(persons[i].sex === 'male' && persons[i].age >= 18){
+      maleAdulte[maleCount] = persons[i];
+      maleCount++;
+    }
+  }
+  answer[0] = femaleAdulte
+  answer[1] = maleAdulte
+  return answer ; 
 }
-
+//console.log(findAdults(persons1))
 module.exports = findAdults;
