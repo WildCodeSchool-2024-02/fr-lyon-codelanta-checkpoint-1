395cc341a7b703d6e9ed82ee173d69fbbc8cf0b0
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-03-25 09:54:26 +0100

    finish Algo1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -6,8 +6,15 @@ thirdAngle(90, 30) doit retourner 60
 thirdAngle(20, 80) doit retourner 80
 */
 
+const a = 90
+const b = 30
+
 function thirdAngle(a, b) {
-  // Your code here !
+  if (thirdAngle === a - b) {
+    return thirdAngle
+  }
+ 
+  
 }
 
 module.exports = thirdAngle;
