dfcdd52ce6989b14e69c8f0c380df81b2cb598fb
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-03-25 11:53:40 +0100

    finished step 3 and 4 of JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -10,8 +10,18 @@ avatar.addEventListener("click", () => {
 
 const promptBtn = document.querySelector("#promptBtn");
 const firstName = document.querySelector("#firstname");
-
+const pinkBg = document.querySelectorAll(".pink-bg");
+const pinkText = document.querySelectorAll(".pink-text");
+console.log(pinkText);
 promptBtn.addEventListener("click", () => {
+  const color = prompt("Enter your color :");
+  for (const element of pinkBg) {
+    element.style.backgroundColor = color;
+  }
+  for (const text of pinkText) {
+    text.style.color = color;
+  }
+  promptBtn.style.backgroundColor = color;
   firstName.innerText = prompt("Enter your name :");
   firstName.style.color = "white";
 });
