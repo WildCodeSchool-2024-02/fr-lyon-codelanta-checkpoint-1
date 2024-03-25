865f8e1981ed46411776805545528ff2d84c5b8a
Thien Nguyen <thien.ngu25@gmail.com>
2024-03-25 09:40:31 +0100

    finish algo1 in part1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,9 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  let c = 180;
+  let lastAngle = c - a - b;
+  return lastAngle;
 }
 
 module.exports = thirdAngle;
