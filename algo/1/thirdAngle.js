a9d444240457ac98eee630ccf5f2701a32b234eb
Bastien Fournier <bastien.fournier06@gmail.com>
2024-03-25 12:29:12 +0100

    premier commit du checkpoint

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -8,6 +8,26 @@ thirdAngle(20, 80) doit retourner 80
 
 function thirdAngle(a, b) {
   // Your code here !
+
+  const sumAngles = a + b;
+
+  const angle3 = 180 - thirdAngle;
+
+  return angle3;
+
+
+
+
 }
+
+  // const a = 30
+  // const b = 60
+  // const c = 180
+  // // const aditionAngles = a - b
+  // const thirdAngle = thirdAngle (a, b, c) 
+  //   if (a === 30 && b === 60) {
+  //     console.log("le 3 eme angles est :" thirdAngle)
+  //   }
+  // console.log("le 3 eme angles : " thirdAngle);
 
 module.exports = thirdAngle;
---
+++ site/js/portfolio.js
@@ -0,0 +1,36 @@
+// const avatarImage = document.getElementsByClassName('.img1');
+// console.log("coucouc")
+// function changeAvatar() {
+    
+//     avatarImage.src = 'image/avatar-bis.png';
+// }
+
+// avatarImage.addEventListener('click', changeAvatar);
+
+    const enterColor = prompt ("rentre la couleur !")
+    const userName = prompt ("Quel est ton prénom ?");
+    const newName = document.getElementById('firstname')
+    const newButton = document.createElement('button')
+    
+
+
+    firstname.innerHTML = userName
+    firstname.style.color = 'white'
+    newButton.classList.add('boutton')
+
+   
+
+    // newButton.textContent = "modifie texte et couleur"
+
+    // p.appendChild(newButton)
+ 
+    // fonction newButton () {
+        
+    //     const nouveauBoutton = document.getElementsByTagName('p')
+    //     const newButton = document.createElement('button')
+    // }
+
+// const userName = prompt ("Quel est ton prénom ?");
+// const newName = document.getElementById(#firstname);
+
+// firstname.innerHTML = "userName"
