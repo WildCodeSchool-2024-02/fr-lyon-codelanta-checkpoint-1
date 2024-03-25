4f2b53dbcdc6f27cb2ac9793e6b167ac3a997b79
YohanGH <YohanGH@proton.me>
2024-03-25 11:55:31 +0100

    Solving exercices JS Step2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,13 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let avatar = document.getElementById('avatarImage');
-    let avatarState = false;
+document.addEventListener("DOMContentLoaded", function () {
+	let avatar = document.getElementById("avatarImage");
+	let avatarState = false;
 
-    avatar.addEventListener('click', function() {
-        if (avatarState) {
-            avatar.src = '../image/avatar.svg';
-        } else {
-            avatar.src = '../image/avatar-bis.png';
-        }
-        avatarState = !avatarState;
-    });
+	avatar.addEventListener("click", function () {
+		if (avatarState) {
+			avatar.src = "../image/avatar.svg";
+		} else {
+			avatar.src = "../image/avatar-bis.png";
+		}
+		avatarState = !avatarState;
+	});
+
+	let Btn = document.getElementById("Btn");
+
+	Btn.addEventListener("click", function () {
+		let newName = prompt("Please enter your name:", "");
+
+		if (newName !== null && newName !== "") {
+			let changeColor = document.getElementById("firstname");
+
+			changeColor.textContent = newName;
+			changeColor.style.color = "white";
+		}
+	});
 });
