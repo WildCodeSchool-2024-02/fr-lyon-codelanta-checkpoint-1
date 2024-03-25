2d67a2a8e36ee973d408d8af2fe761d5919da883
Adeline <149504531+kna1703@users.noreply.github.com>
2024-03-25 12:59:35 +0100

    modifications portfolio

---
+++ site/js/portfolio.js
@@ -0,0 +1,10 @@
+const avatar = document.querySelector('.avatar');
+
+avatar.addEventListener('click', function () {
+    avatar.src = "../site/image/avatar.svg"
+});
+
+const name = document.querySelector('#firstname');
+avatar.addEventListener('click', function () {
+    const yourName = prompt("What is your name ? ");
+});
